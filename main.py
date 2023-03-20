18c5304606478de385654aff078c4358e0f6058f
Praveen Soman <praveens79@gmail.com>
2023-03-20 22:09:25 +0100

    midleware added

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from db import database
 from resources.routes import api_router
 
 app = FastAPI()
 app.include_router(api_router)
+origins = [
+    "http://localhost",
+    "http://localhost:4200"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
 
+)
 
 @app.on_event("startup")
 async def startup():
@@ -15,5 +27,3 @@ async def startup():
 @app.on_event("shutdown")
 async def shutdown():
     await database.disconnect()
-
-
